22023e185818c74b24e5c115e2b6100430257d1a
Lucas <lucas.koepke98@gmail.com>
2024-04-16 18:52:57 +0200

    test commit

--- src/main/java/org/hbrs/se1/ss24/uebung1/client/Client.java
+++ src/main/java/org/hbrs/se1/ss24/uebung1/client/Client.java
@@ -2,8 +2,7 @@ package org.hbrs.se1.ss24.uebung1.client;
 
 public class Client {
     public void printTransformation(int number) {
-        String result = Objekterzeugung.Objekt(number) ;
-        System.out.println("Test");
+        String result = Objekterzeugung.Objekt(number);
         System.out.println("Die römische Schreibweise der Zahl " + number + " ist: " + result);
     }
 }
