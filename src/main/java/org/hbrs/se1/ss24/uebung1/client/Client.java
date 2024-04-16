d583dabf6a8d8377eb4d218f4261d0189b54fea7
Lucas <lucas.koepke98@gmail.com>
2024-04-16 20:28:35 +0200

    answered first 3 questions

--- src/main/java/org/hbrs/se1/ss24/uebung1/client/Client.java
+++ src/main/java/org/hbrs/se1/ss24/uebung1/client/Client.java
@@ -1,5 +1,7 @@
 package org.hbrs.se1.ss24.uebung1.client;
 
+import org.hbrs.se1.ss24.uebung1.businesslogic.Objekterzeugung;
+
 public class Client {
     public void printTransformation(int number) {
         String result = Objekterzeugung.Objekt(number);
